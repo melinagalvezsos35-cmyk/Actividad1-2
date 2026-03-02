4361fda55e8a0b084c522419d42564e7f51afb57
Melina <melinagalvezsos35@gmail.com>
2026-03-01 21:05:10 -0500

    refactorizado

--- ProgramacionEstructurada22/Program.cs
+++ ProgramacionEstructurada22/Program.cs
@@ -1,116 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class Program
 {
-     static void Main(string[] args)
+    static void Main(string[] args)
+    {
+        
+        int cantidadPersonas = LeerEntero("Ingresa la cantidad de personas a clasificar: ");
+
+        if (cantidadPersonas == 1)
         {
-            int cantidadPersonas;
-            int edadPersona;
-            string nombrePersona;
+            Console.Write("Ingresa el nombre de la persona: ");
+            string nombrePersona = Console.ReadLine();
 
-            Console.Write("Ingresa la cantidad de personas a clasificar: ");
+          
+            int edadPersona = LeerEntero("Ingresa la edad: ");
 
-
-            while (!int.TryParse(Console.ReadLine(), out cantidadPersonas)|| cantidadPersonas < 1)
+        
+            if (EsMayor(edadPersona))
             {
-              Console.WriteLine("La cantidad debe ser numerico, no letras o simbolos");
-              Console.Write("Ingresa la cantidad de personas a clasificar: ");
-            }
-
-            if (cantidadPersonas == 1)
-            {
-                Console.Write("Ingresa el nombre de la persona: "); 
-                nombrePersona = Console.ReadLine();
-
-
-                Console.Write("Ingresa la edad: ");
-                while (!int.TryParse(Console.ReadLine(), out edadPersona))
-                {
-                    Console.WriteLine("La edad debe ser numerico, no letras o simbolos");
-                    Console.Write("Ingresa la edad: ");
-                }
-
-
-
-                if (edadPersona >= 18)
-                {
-                    Console.WriteLine(nombrePersona +" Tiene "+ edadPersona+" años"+ "-"+" Es mayor de edad"); 
-                }
-                else
-                {
-                    Console.WriteLine(nombrePersona +" Tiene "+ edadPersona+" años"+ "-"+" Es menor de edad"); 
-                }
-
-
+                Console.WriteLine($"{nombrePersona} Tiene {edadPersona} años - Es mayor de edad");
             }
             else
             {
-                List<string> nombreGeneral = new List<string>();
-                List<int> edadGeneral = new List<int>();
+                Console.WriteLine($"{nombrePersona} Tiene {edadPersona} años - Es menor de edad");
+            }
+        }
+        else
+        {
+            List<string> nombreGeneral = new List<string>();
+            List<int> edadGeneral = new List<int>();
+            List<string> nombresMayores = new List<string>();
+            List<int> edadesMayores = new List<int>();
+            List<string> nombresMenores = new List<string>();
+            List<int> edadesMenores = new List<int>();
 
-                List<string> nombresPersonasMayores = new List<string>(); 
-                List<int> edadesPersonasMayores = new List<int>();
+            for (int i = 1; i <= cantidadPersonas; i++)
+            {
+                Console.Write($"Ingresa el nombre de la persona {i} = ");
+                string nombre = Console.ReadLine();
+                int edad = LeerEntero($"Ingresa la edad {i} = ");
 
-                List<string> nombresPersonasMenores = new List<string>();
-                List<int> edadesPersonasMenores = new List<int>();
+                nombreGeneral.Add(nombre);
+                edadGeneral.Add(edad);
 
-                for (int i = 1; i <= cantidadPersonas; i++)
+               
+                if (EsMayor(edad))
                 {
-
-                    Console.Write("Ingresa el nombre de la persona " + i + " = ");
-                    nombrePersona = Console.ReadLine();
-                    Console.Write("ingresa la edad " + i + " = ");
-                    while (!int.TryParse(Console.ReadLine(), out edadPersona))
-                    {
-                        Console.WriteLine("La edad debe ser numérica, no simbolos o letra");
-                        Console.Write("Ingresa la edad correcta: ");
-                    }
-
-                    nombreGeneral.Add(nombrePersona);
-                    edadGeneral.Add(edadPersona);
-
-                    if (edadPersona >= 18)
-                    {
-                        nombresPersonasMayores.Add(nombrePersona); 
-                        edadesPersonasMayores.Add(edadPersona);
-                    }
-                    else
-                    {
-                        nombresPersonasMenores.Add(nombrePersona);
-                        edadesPersonasMenores.Add(edadPersona);
-                    }
+                    nombresMayores.Add(nombre);
+                    edadesMayores.Add(edad);
                 }
-
-                Console.WriteLine("---- Lista GeneralL ----");
-                Console.WriteLine(" ");
-                for (int i = 0; i < nombreGeneral.Count; i++)
+                else
                 {
-                    Console.WriteLine(nombreGeneral[i] + " - " + edadGeneral[i] + " años");
-                }
-
-                if (nombresPersonasMayores.Count > 0)
-                {
-
-                   Console.WriteLine("----Personas Mayores----");
-                   Console.WriteLine(" ");
-                   for (int i = 0; i < nombresPersonasMayores.Count; i++) 
-                   {
-                    Console.WriteLine(nombresPersonasMayores[i] + " - " + edadesPersonasMayores[i]+ " años");
-                   }
-                }
-
-                if (nombresPersonasMenores.Count > 0)
-                {
-                   Console.WriteLine("----PERSONAS MENORES----");
-                   Console.WriteLine(" ");
-                   for (int i = 0; i < nombresPersonasMenores.Count; i++)
-                   {
-                    Console.WriteLine(nombresPersonasMenores[i] + " - " + edadesPersonasMenores[i]+ " años");
-                   }
+                    nombresMenores.Add(nombre);
+                    edadesMenores.Add(edad);
                 }
             }
 
+        
+            ImprimirListas(nombreGeneral, edadGeneral, nombresMayores, edadesMayores, nombresMenores, edadesMenores);
         }
     }
+
+   
+
+    
+    static int LeerEntero(string mensaje)
+    {
+        int valor;
+        Console.Write(mensaje);
+        while (!int.TryParse(Console.ReadLine(), out valor) || valor < 1)
+        {
+            Console.WriteLine("La edad debe ser numérica, no símbolos o letra");
+            Console.Write("Intente de nuevo: ");
+        }
+        return valor;
+    }
+
+
+    static bool EsMayor(int edad)
+    {
+        return edad >= 18;
+    }
+
+       static void ImprimirListas(List<string> genNom, List<int> genEd, List<string> mayNom, List<int> mayEd, List<string> menNom, List<int> menEd)
+    {
+        Console.WriteLine("\n---- Lista General ----");
+        for (int i = 0; i < genNom.Count; i++)
+        {
+            Console.WriteLine($"{genNom[i]} - {genEd[i]} años");
+        }
+
+        if (mayNom.Count > 0)
+        {
+            Console.WriteLine("\n---- Personas Mayores ----");
+            for (int i = 0; i < mayNom.Count; i++)
+            {
+                Console.WriteLine($"{mayNom[i]} - {mayEd[i]} años");
+            }
+        }
+
+        if (menNom.Count > 0)
+        {
+            Console.WriteLine("\n---- PERSONAS MENORES ----");
+            for (int i = 0; i < menNom.Count; i++)
+            {
+                Console.WriteLine($"{menNom[i]} - {menEd[i]} años");
+            }
+        }
+    }
+}
